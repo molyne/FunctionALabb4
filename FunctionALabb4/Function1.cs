6c6d369cfe68a31ed2902892e8864ebf06f056e3
Molyn Eriksson <molyneriksson@gmail.com>
2018-01-28 16:48:01 +0100

    selected pending pictures instead

--- FunctionALabb4/Function1.cs
+++ FunctionALabb4/Function1.cs
@@ -19,39 +19,39 @@ namespace FunctionALabb4
             log.Info("C# HTTP trigger function processed a request.");
 
             // parse query parameter
-            string email = req.GetQueryNameValuePairs()
-                .FirstOrDefault(q => string.Compare(q.Key, "email", true) == 0)
+            string pictureURL = req.GetQueryNameValuePairs()
+                .FirstOrDefault(q => string.Compare(q.Key, "pictureURL", true) == 0)
                 .Value;
 
             // Get request body
             dynamic data = await req.Content.ReadAsAsync<object>();
 
-            email = email ?? data?.email;
+            pictureURL = pictureURL ?? data?.pictureURL;
 
-            var customer = GetCustomer(email);
-            return req.CreateResponse(HttpStatusCode.OK, customer, "application/json");
+            var picture = GetPicture(pictureURL);
+            return req.CreateResponse(HttpStatusCode.OK, picture, "application/json");
 
 
         }
 
-        private static List<User> GetCustomer(string email)
+        private static List<Picture> GetPicture(string email)
         {
 
             string EndpointUrl = "https://picturesdb.documents.azure.com:443/";
             string PrimaryKey = "cHRKIwWfOVFQOxDG8h33OIr0YoIpWZQRe3G1DF7ha43ZfxVhr7Ev8wdc0wgvMUpDoCWsI50dYrOlpswocncohg==";
             string databaseName = "PicturesDB";
-            string collectionName = "Users";
+            string collectionName = "Pending pictures";
 
             var client = new DocumentClient(new Uri(EndpointUrl), PrimaryKey);
             FeedOptions queryOptions = new FeedOptions { MaxItemCount = -1, EnableCrossPartitionQuery = true };
 
-            var query = client.CreateDocumentQuery<User>(
+            var query = client.CreateDocumentQuery<Picture>(
                     UriFactory.CreateDocumentCollectionUri(databaseName, collectionName),
-                    "SELECT * FROM Users",
+                    "SELECT * FROM Pending pictures",
                     queryOptions);
 
-            var user = query.ToList();
-            return user;
+            var picture = query.ToList();
+            return picture;
 
         }
     }
