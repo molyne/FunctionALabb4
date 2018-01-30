ff2edac08690fbce63736426fede5aca6fdb4629
Molyn Eriksson <molyneriksson@gmail.com>
2018-01-30 21:42:16 +0100

    Now you can approve pending pictures. And I cleaned the code

--- FunctionALabb4/Function1.cs
+++ FunctionALabb4/Function1.cs
@@ -13,6 +13,20 @@ namespace FunctionALabb4
 {
     public static class Function1
     {
+        static string EndpointUrl = "https://picturesdb.documents.azure.com:443/";
+        static string PrimaryKey = "cHRKIwWfOVFQOxDG8h33OIr0YoIpWZQRe3G1DF7ha43ZfxVhr7Ev8wdc0wgvMUpDoCWsI50dYrOlpswocncohg==";
+        static DocumentClient client = new DocumentClient(new Uri(EndpointUrl), PrimaryKey);
+
+
+        static string databaseName = "PicturesDB";
+        static string pendingCollection = "Pending pictures";
+        static string reviewedCollection = "Reviewed pictures";
+
+        //connections to collections
+        static Uri pendingCollectionLink = UriFactory.CreateDocumentCollectionUri(databaseName, pendingCollection);
+        static Uri reviewedCollectionLink = UriFactory.CreateDocumentCollectionUri(databaseName, reviewedCollection);
+
+
         [FunctionName("Function1")]
         public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
         {
@@ -53,60 +67,41 @@ namespace FunctionALabb4
                 return req.CreateResponse(HttpStatusCode.BadRequest, "Please enter mode.");
             }
 
-
-
         }
 
         private static List<Picture> GetPicture(string email)
         {
-
-            string EndpointUrl = "https://picturesdb.documents.azure.com:443/";
-            string PrimaryKey = "cHRKIwWfOVFQOxDG8h33OIr0YoIpWZQRe3G1DF7ha43ZfxVhr7Ev8wdc0wgvMUpDoCWsI50dYrOlpswocncohg==";
-            string databaseName = "PicturesDB";
-            string collectionName = "Pending pictures";
-            string toCollectionName = "Reviewed pictures";
-
-            var client = new DocumentClient(new Uri(EndpointUrl), PrimaryKey);
+            
             FeedOptions queryOptions = new FeedOptions { MaxItemCount = -1, EnableCrossPartitionQuery = true };
 
-            var query = client.CreateDocumentQuery<Picture>(
-                    UriFactory.CreateDocumentCollectionUri(databaseName, collectionName),
-                    "SELECT * FROM Pending pictures",
+            var printPending = client.CreateDocumentQuery<Picture>(
+                pendingCollectionLink, "SELECT * FROM Pending pictures",
                     queryOptions);
 
-            var picture = query.ToList();
+            var picture = printPending.ToList();
             return picture;
 
         }
-        private static string ApprovePicture(string selectedId)
+        private static async Task<string> ApprovePicture(string selectedId)
         {
-            string EndpointUrl = "https://picturesdb.documents.azure.com:443/";
-            string PrimaryKey = "cHRKIwWfOVFQOxDG8h33OIr0YoIpWZQRe3G1DF7ha43ZfxVhr7Ev8wdc0wgvMUpDoCWsI50dYrOlpswocncohg==";
-            string databaseName = "PicturesDB";
-            string fromCollectionName = "Pending pictures";
-            string toCollectionName = "Reviewed pictures";
-
-            var client = new DocumentClient(new Uri(EndpointUrl), PrimaryKey);
-
-            FeedOptions queryOptions = new FeedOptions { MaxItemCount = -1, EnableCrossPartitionQuery = true };
-
-       
-
-            var collectionLink = UriFactory.CreateDocumentCollectionUri(databaseName, fromCollectionName);
-            var collectionLink2 = UriFactory.CreateDocumentCollectionUri(databaseName, toCollectionName);
-
-            var query = client.CreateDocumentQuery<Picture>(collectionLink)
+            //hittar id man valt i webbläsaren
+            var picture = client.CreateDocumentQuery<Picture>(pendingCollectionLink)
                                .Where(r => r._id == selectedId)
                               .AsEnumerable()
                               .SingleOrDefault();
-            client.CreateDocumentQuery<Picture>(collectionLink2, $"INSERT INTO [Reviewed pictures] (_id,PictureURL) VALUES ({query._id},{query.PictureURL})");
-//             client.UpsertDocument(collectionLink2, query).RunSynchronously();
 
+            //lägger till valda dokumentet i reviewed pictures
+            await client.CreateDocumentAsync(reviewedCollectionLink, picture);
 
+            //tar bort det valda dokumentet från pending pictures
+            await client.DeleteDocumentAsync(UriFactory.CreateDocumentUri(databaseName, pendingCollection, selectedId));
 
 
             //ändra så den returner om det lyckades eller inte
             return "hej";
         }
+       
+
+
     }
 }
